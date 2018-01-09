841d76c838266b15d32529561b1081065d55adb3
krzysieksad <krzysiek.sadlak@gmail.com>
2018-01-10 00:22:39 +0100

    Added checkstyle. Added new class Calculator. Added test of Calculator methods in TestingMain.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,21 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        if (calculator.add(5,17) == 22) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test FAILED");
+        }
+
+        if (calculator.subtract(51,36) == 15) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test FAILED");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public int add(final int addendA, final int addendB) {
+        return addendA + addendB;
+    }
+    public int subtract(final int minuend, final int subtrahend) {
+        return minuend - subtrahend;
+    }
+}
