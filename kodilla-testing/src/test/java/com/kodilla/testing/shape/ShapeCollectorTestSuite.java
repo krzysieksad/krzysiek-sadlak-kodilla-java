5b237a3b37585c1e1308dce330eeef580e8c05d9
krzysieksad <krzysiek.sadlak@gmail.com>
2018-01-12 22:28:49 +0100

    Task 6.5. Added given/when/then comments to unit tests.

--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -1,60 +1,86 @@
 package com.kodilla.testing.shape;
 
 import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ShapeCollectorTestSuite {
     @Test
     public void testAddFigure() {
+        //given
         ShapeCollector shapeCollector = new ShapeCollector();
+        //when
         shapeCollector.addFigure(new Triangle(5, 3));
+        //then
         Assert.assertEquals(shapeCollector.showFigures().size(), 1);
     }
 
     @Test
     public void testRemoveFigure() {
+        //given
         ShapeCollector shapeCollector = new ShapeCollector();
         shapeCollector.addFigure(new Triangle(5, 3));
+        //when
         boolean removal = shapeCollector.removeFigure(new Triangle(5, 3));
+        //then
         Assert.assertTrue(removal);
         Assert.assertEquals(shapeCollector.showFigures().size(), 0);
     }
 
     @Test
     public void testRemoveNonExistingFigure() {
+        //given
         ShapeCollector shapeCollector = new ShapeCollector();
+        //when
         boolean removal = shapeCollector.removeFigure(new Triangle(5, 3));
+        //then
         Assert.assertFalse(removal);
     }
 
     @Test
     public void testGetFigure() {
+        //given
         ShapeCollector shapeCollector = new ShapeCollector();
         Shape newFigure = new Triangle(5, 3);
+        //when
         shapeCollector.addFigure(newFigure);
+        //then
         Assert.assertEquals(newFigure, shapeCollector.getFigure(0));
     }
 
     @Test
     public void testShowFigures() {
+        //given
         ShapeCollector shapeCollector = new ShapeCollector();
         Shape newTriangle = new Triangle(5, 3);
         shapeCollector.addFigure(newTriangle);
         Shape newSquare = new Square(6.8);
         shapeCollector.addFigure(newSquare);
-        Assert.assertEquals(shapeCollector.showFigures(), Arrays.asList(newTriangle, newSquare));
+        //when
+        List<Shape> figures = shapeCollector.showFigures();
+        //then
+        Assert.assertEquals(figures, Arrays.asList(newTriangle, newSquare));
     }
 
     @Test
     public void testGetShapeName() {
+        //given
         Shape newFigure = new Circle(3);
-        Assert.assertEquals(newFigure.getShapeName(), "Circle");
+        //when
+        String figureName = newFigure.getShapeName();
+        //then
+        Assert.assertEquals(figureName, "Circle");
     }
 
     @Test
     public void testGetField() {
+        //given
         Shape square = new Square(8);
-        Assert.assertEquals(square.getField(), 64);
+        //when
+        double field = square.getField();
+        //then
+        Assert.assertEquals(field, 64, 0);
     }
 }
