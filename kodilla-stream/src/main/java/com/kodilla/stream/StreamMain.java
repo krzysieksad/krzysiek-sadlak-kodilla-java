b00a0335a669289f05f1d3dc9f5ef31244902ede
krzysieksad <krzysiek.sadlak@gmail.com>
2018-01-21 09:40:36 +0100

    Task 7.1. Fixed.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,18 +1,15 @@
 package com.kodilla.stream;
 
-//In submodule 7.3. everything in this class should have been deleted
-/*import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.reference.FunctionalCalculator;*/
-
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+//import com.kodilla.stream.lambda.ExpressionExecutor;
+//import com.kodilla.stream.reference.FunctionalCalculator;
+//import com.kodilla.stream.book.Book;
+//import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
-
+import com.kodilla.stream.iterate.NumbersGenerator;
 import java.time.LocalDate;
-import java.util.List;
+//import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 //import com.kodilla.stream.person.People;
@@ -23,6 +20,7 @@ public class StreamMain {
      * @param args just args
      */
     public static void main(final String[] args) {
+        //Task 7.3
         Forum forum = new Forum();
         Map<Integer, ForumUser> users = forum.getUserList().stream()
                 .filter(user -> user.getSex() == 'M')
@@ -77,7 +75,7 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);*/
 
         System.out.println("");
 
@@ -90,13 +88,13 @@ public class StreamMain {
         poemBeautifier.beautify("Magic growth", String::toUpperCase);
         System.out.println("");
         poemBeautifier.beautify("Creating Haiku\nIs harder than it appears\nThis may take a while",
-                                (poem) -> ("try reading that: " + poem).replaceAll("[\n :]", "").toLowerCase());
+            (poem) -> ("try reading that: " + poem).replaceAll("[\n :]", "").toLowerCase());
         System.out.println("");
         poemBeautifier.beautify("Raindrops on this page\nWind blows my paper away\nOh crap! I need that...\n", (poem) -> "I hate poems!");
 
         System.out.println("\nSubmodule 7.2.:");
 
         System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);*/
+        NumbersGenerator.generateEven(20);
     }
 }
