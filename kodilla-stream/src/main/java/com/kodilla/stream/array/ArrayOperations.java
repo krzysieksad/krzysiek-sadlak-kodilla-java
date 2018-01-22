6da31acc31f4e0345b1f5b3b5d7069f1b9af84d1
krzysieksad <krzysiek.sadlak@gmail.com>
2018-01-22 23:22:25 +0100

    Task 7.5. Finished

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,7 +1,17 @@
 package com.kodilla.stream.array;
 
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
 public interface ArrayOperations {
-    static double getAverage(int[] numbers) {
-        return 0D;
+    /**
+     * Counts average of numbers from table.
+     * @param numbers table.
+     * @return double.
+     */
+    static OptionalDouble getAverage(int[] numbers) {
+        return IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average();
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,7 +1,6 @@
 package com.kodilla.stream.array;
 
-import static com.kodilla.stream.array.ArrayOperations.getAverage;
-
+import java.util.OptionalDouble;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,9 +11,9 @@ public class ArrayOperationsTestSuite {
         int[] numbers = {3,4,75,5,43,945,652,76,87,75,413,1,1423,67,65,48,676,65,1,42145,265,63787,68};
 
         //when
-        double average = getAverage(numbers);
+        OptionalDouble average = ArrayOperations.getAverage(numbers);
 
         //then
-        Assert.assertEquals(4825.60869, average, 0.00001);
+        Assert.assertEquals(4825.60869, average.getAsDouble(), 0.00001);
     }
 }
