36aae6a86d5776aa0c3ea698583b4b8a4d6f2ac9
krzysieksad <krzysiek.sadlak@gmail.com>
2018-01-25 18:16:57 +0100

    Task 7.5. Fixed.

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -11,9 +11,9 @@ public class ArrayOperationsTestSuite {
         int[] numbers = {3,4,75,5,43,945,652,76,87,75,413,1,1423,67,65,48,676,65,1,42145,265,63787,68};
 
         //when
-        OptionalDouble average = ArrayOperations.getAverage(numbers);
+        double average = ArrayOperations.getAverage(numbers).getAsDouble();
 
         //then
-        Assert.assertEquals(4825.60869, average.getAsDouble(), 0.00001);
+        Assert.assertEquals(4825.60869, average, 0.00001);
     }
 }
