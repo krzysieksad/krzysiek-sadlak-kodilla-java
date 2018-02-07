fe526af8924da1da752f92b568bb49d1277113dd
krzysieksad <krzysiek.sadlak@gmail.com>
2018-02-07 23:34:16 +0100

    Task 9.5. Finished 2/3 methods in FlightFinder.

--- kodilla-good-patterns/src/main/java/com/kodilla/flightcompany/FlightFinder.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/flightcompany/FlightFinder.java
@@ -1,20 +1,39 @@
 package com.kodilla.flightcompany;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FlightFinder {
     private final FlightOffers companyFlights;
 
-    FlightFinder(FlightOffers companyFlights) {
+    FlightFinder(final FlightOffers companyFlights) {
         this.companyFlights = companyFlights;
     }
 
+    /**
+     * Get all destination cities.
+     * @param city departure city
+     * @return list of destinations
+     */
     public List<String> findFlightsFromCity(final String city) {
-        return null;
+        return companyFlights.getFlightList().stream()
+                .filter(flight -> flight.getFlightFrom().equals(city))
+                .map(Flight::getFlightTo)
+                .sorted()
+                .collect(Collectors.toList());
     }
 
+    /**
+     * Get all departure cities.
+     * @param city arrival city
+     * @return list of departures
+     */
     public List<String> findFlightsToCity(final String city) {
-        return null;
+        return companyFlights.getFlightList().stream()
+                .filter(flight -> flight.getFlightTo().equals(city))
+                .map(Flight::getFlightFrom)
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     public List<String> findFlightsFromCityToCity(final String fromCity, final String toCity, final int maxConnections) {
