e983a51e91fae73a0aa0e18117bc188deb4a23d7
krzysieksad <krzysiek.sadlak@gmail.com>
2018-02-26 17:35:41 +0100

    Task 12.6. Used 'try with resources'.

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -2,11 +2,21 @@ package com.kodilla.jdbc;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DbManagerTestSuite {
+    private int countResultSet(final ResultSet resultSet) throws SQLException {
+        resultSet.beforeFirst();
+        int counter = 0;
+        while (resultSet.next()) {
+            counter++;
+        }
+        return counter;
+    }
+
     @Test
     public void testConnect() throws SQLException {
         //Given
@@ -23,20 +33,17 @@ public class DbManagerTestSuite {
 
         //When
         String sqlQuery = "SELECT * FROM USERS";
-        Statement statement = dbManager.getConnection().createStatement();
-        ResultSet rs = statement.executeQuery(sqlQuery);
+        try (Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery)) {
 
-        //Then
-        int counter = 0;
-        while (rs.next()) {
-            System.out.println(rs.getInt("ID") + ", " +
-                    rs.getString("FIRSTNAME") + ", " +
-                    rs.getString("LASTNAME"));
-            counter++;
+            //Then
+            while (rs.next()) {
+                System.out.println(rs.getInt("ID") + ", " +
+                        rs.getString("FIRSTNAME") + ", " +
+                        rs.getString("LASTNAME"));
+            }
+            Assert.assertEquals(5, countResultSet(rs));
         }
-        rs.close();
-        statement.close();
-        Assert.assertEquals(5, counter);
     }
 
     @Test
@@ -50,18 +57,14 @@ public class DbManagerTestSuite {
                 "join posts p on p.USER_ID = u.ID\n" +
                 "group by p.USER_ID\n" +
                 "having count(p.USER_ID) > 1;";
-        Statement statement = dbManager.getConnection().createStatement();
-        ResultSet rs = statement.executeQuery(sqlQuery);
-
-        //Then
-        int counter = 0;
-        while (rs.next()) {
-            System.out.println(rs.getString("FIRSTNAME") + ", " +
-                    rs.getString("LASTNAME"));
-            counter++;
+        try (final Statement statement = dbManager.getConnection().createStatement();
+             final ResultSet resultSet = statement.executeQuery(sqlQuery)) {
+            //Then
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("FIRSTNAME") + ", " +
+                        resultSet.getString("LASTNAME"));
+            }
+            Assert.assertEquals(2, countResultSet(resultSet));
         }
-        rs.close();
-        statement.close();
-        Assert.assertEquals(2, counter);
     }
 }
